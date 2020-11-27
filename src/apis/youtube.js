7a79ffeffa966590dfa2d9968d1252cbb4baaa8d
Clucas0311 <clucas0311@gmail.com>
2020-11-27 18:46:29 -0500

    fixing API

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const KEY = 'AIzaSyCbmKz8Uqfykgc4Nb5l3lkaoxZ8BK1vlG8';
+const KEY = 'AIzaSyBejaIBK5CoVsGr6mCvDkvryyHqyimUVKQ';
 
 export default axios.create({
 	baseURL: 'https://www.googleapis.com/youtube/v3',
