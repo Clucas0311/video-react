1cf145261c39a983bdc317644fc944267f97d0c6
Clucas0311 <clucas0311@gmail.com>
2020-11-26 21:54:42 -0500

    Grabbing list of videos

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
 class App extends React.Component {
-	onTermSubmit = (term) => {
-		youtube.get('/search', {
+	state = { videos: [] };
+
+	onTermSubmit = async (term) => {
+		const response = await youtube.get('/search', {
 			params: {
 				q: term
 			}
 		});
+		this.setState({ videos: response.data.items });
 	};
 	render() {
 		return (
