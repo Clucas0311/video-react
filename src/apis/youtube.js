b0ef2aabf1b43ec24f82502d4a8f5c60cf381358
Clucas0311 <clucas0311@gmail.com>
2020-11-26 21:37:43 -0500

    Adding api

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,1 +1,12 @@
+import axios from 'axios';
+
 const KEY = 'AIzaSyCbmKz8Uqfykgc4Nb5l3lkaoxZ8BK1vlG8';
+
+export default axios.create({
+	baseURL: 'https://www.googleapis.com/youtube/v3',
+	params: {
+		part: 'snippet',
+		maxResults: 5,
+		key: KEY
+	}
+});
